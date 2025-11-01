014872638f0193d91e21225bc5bd1f7f57605a9d
OptiMus-cyber <rautelaprakash543@gmail.com>
2025-11-01 13:44:10 +0530

    Day 143 (Leetcode Daily Practice)

---
+++ Leetcode DPP/Day 0143 - 3217/Delete Nodes From Linked List Present in Array.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++) mp[nums[i]]++;
+        while(head&&mp.count(head->val)) head=head->next;
+        ListNode* temp=head;
+        while(temp&&temp->next) {
+            if(mp.count(temp->next->val)) {
+                temp->next=temp->next->next;
+            } else temp=temp->next;
+        }
+        return head;
+    }
+};
