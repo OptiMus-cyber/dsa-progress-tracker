2a4892f8405c9a7dd8a1d058c8d1c10788a68ec0
OptiMus-cyber <rautelaprakash543@gmail.com>
2025-11-10 16:45:44 +0530

    Day 152 (Leetcode Daily Practice)

---
+++ Leetcode DPP/Day 0152 - 3542/Minimum Operations to Convert All Elements to Zero.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+       int ans = 0;
+        stack<int> st;
+
+        for (int x : nums) {
+            while (!st.empty() && st.top() > x) {
+                st.pop();
+                ans++;
+            }
+            if (x > 0 && (st.empty() || st.top() < x)) {
+                st.push(x);
+            }
+        }
+
+        while (!st.empty()) {
+            st.pop();
+            ans++;
+        }
+
+        return ans;
+    }
+};
