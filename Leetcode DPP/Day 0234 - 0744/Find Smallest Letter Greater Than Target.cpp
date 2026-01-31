6e8d46b4a625f69f1c803647abec0433b05a00e6
OptiMus-cyber <rautelaprakash543@gmail.com>
2026-01-31 10:23:39 +0530

    Day 234 (Leetcode Daily Practice)

---
+++ Leetcode DPP/Day 0234 - 0744/Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int l=0, r=letters.size()-1;
+        while(l<=r) {
+            int mid=(r-l)/2+l;
+            if(letters[mid]<=target) {
+                l=mid+1;
+            } else {
+                r=mid-1;
+            }
+        }
+        if(l==letters.size()) return letters[0];
+        return letters[l];
+    }
+};
