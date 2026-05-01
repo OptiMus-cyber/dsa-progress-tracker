ab141f721afc5c271d7dc744dab01a843bfafccf
OptiMus-cyber <rautelaprakash543@gmail.com>
2026-05-01 23:58:41 +0530

    Day 324 (Leetcode Daily Practice)

--- Leetcode DPP/Day 0324 - 0396/Rotate Function.cpp
+++ Leetcode DPP/Day 0324 - 0396/Rotate Function.cpp
@@ -1,1 +1,16 @@
-Rotate Function
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int maxi=0, sum=0;
+        for(int i=0; i<nums.size(); i++) {
+            maxi+=i*nums[i];
+            sum+=nums[i];
+        }
+        int last=maxi, n=nums.size();
+        for(int i=0; i<nums.size(); i++) {
+            maxi=max(maxi, last+sum-n*nums[n-i-1]);
+            last=last+sum-n*nums[n-i-1];
+        }
+        return maxi;
+    }
+};
