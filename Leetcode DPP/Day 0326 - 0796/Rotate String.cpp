eba11667a3c256bc37a100619ea3625644b0f9d8
OptiMus-cyber <rautelaprakash543@gmail.com>
2026-05-03 12:49:36 +0530

    Day 326 (Leetcode Daily Practice)

---
+++ Leetcode DPP/Day 0326 - 0796/Rotate String.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.size();
+        for(int i=0; i<s.size(); i++) {
+            if((s.substr(i, n-i)+s.substr(0, i))==goal) return true;
+        }
+        return false;
+    }
+};
