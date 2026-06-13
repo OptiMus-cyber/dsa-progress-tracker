2d633d9c6228d19cb12766be085060c7713a9153
OptiMus-cyber <rautelaprakash543@gmail.com>
2026-06-13 19:42:24 +0530

    Day 367 (Leetcode Daily Practice)

---
+++ Leetcode DPP/Day 0367 - 3838/Weighted Word Mapping.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string s="";
+        for(int i=0; i<words.size(); i++) {
+            int sum=0;
+            for(int j=0; j<words[i].size(); j++) {
+                sum+=weights[words[i][j]-'a'];
+            }
+            sum%=26;
+            s+=('z'-sum);
+        }
+        return s;
+    }
+};
