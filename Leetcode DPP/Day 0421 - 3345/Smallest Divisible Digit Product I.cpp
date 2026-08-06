f9eda39ac1bd91168c4f64c178d4e5200428e073
OptiMus-cyber <rautelaprakash543@gmail.com>
2026-08-06 19:55:53 +0530

    Day 421 (Leetcode Daily Practice)

---
+++ Leetcode DPP/Day 0421 - 3345/Smallest Divisible Digit Product I.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        for(int i=n; ; i++) {
+            int x=i, p=1;
+            while(x) {
+                p*=(x%10);
+                x/=10;
+            }
+            if(p%t==0) return i;
+        }
+        return 0;
+    }
+};
